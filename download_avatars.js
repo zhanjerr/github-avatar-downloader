40484358efdc566ecf2573f06c0d580f1597ac26
zhanjerr <jerry.h.zhan@gmail.com>
2017-04-10 23:27:54 +0000

    got the json, but couldn't use promises

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,44 @@
 var request = require('request');
+var GITHUB_USER = "zhanjerr";
+var GITHUB_TOKEN = "afb30523e97255f70672386d0110e2d6ca0e0132";
 
-console.log("Welcome to the GitHub Avatar Downloader!");
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = {
+    url : `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers : {
+      'User-Agent' : 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+
+  request (requestURL, (error, response, body) =>{
+    if (error){
+      cb(error);
+      return;
+    };
+
+    if (response.statusCode == 200){
+      var json = JSON.parse(body);
+      console.log(json);
+    } else if (response.statusCode == 404) {
+      console.log("Object not found")
+    }
+  });
+  // request.get(requestURL)
+  //   .on('response', function(response){
+  //     if(response.statusCode === 200){
+  //       console.log(body);
+  //     } else{
+  //       console.log("error");
+  //     }
+  //     // console.log(response.statusCode);
+  //   })
+  //   .on('error',function(error){
+  //     cb(error,"");
+  //     return;
+  //   })
+};
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
